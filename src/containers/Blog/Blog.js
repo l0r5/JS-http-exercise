6c41ca13e17b19d2600e6daa34630682bb3a56ef
l0r5 <lars-braitinger@web.de>
2021-04-05 13:52:01 +0200

    Update Fullpost on click and fetch post by post id

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -27,11 +28,16 @@ class Blog extends Component {
             });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render() {
         const posts = this.state.posts.map(post => {
             return <Post key={post.id}
                          title={post.title}
-                         author={post.author}/>
+                         author={post.author}
+                         clicked={() => this.postSelectedHandler(post.id)}/>
         });
         return (
             <div>
@@ -39,7 +45,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost/>
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost/>
